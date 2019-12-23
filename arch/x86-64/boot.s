f20e9930ddefd8d59f0adbc90f9acd2fb02185f9
Clay McClure <clay@daemons.net>
2019-12-22 22:13:41 -0800

    x86-64: Set up boot GDT
    To enter 64-bit long mode we need to add a 64-bit code segment to the
    global descriptor table. But according to the Multiboot specification
    the value in GDTR may be invalid -- the GDT set up by the boot loader
    may have been overwritten. We need to set up our own GDT.
    
    The GDT is an array of segment descriptors, each of which defines the
    base address, limit (size), and flags associated with a segment. In
    addition to the required "null" segment, we define two "flat" 32-bit
    segments (an executable code segment and a read-write data segment)
    spanning the 4 GiB linear address space.
    
    After loading GDTR with the size and address of our GDT, we load all
    segment registers with selectors pointing to our new segments.

--- arch/x86-64/boot.s
+++ arch/x86-64/boot.s
@@ -38,6 +38,26 @@
 	.global _start
 _start:
 	/*
+	 * Set up the GDT.
+	 */
+	lgdt	GDTR
+
+	/*
+	 * Load CS.
+	 */
+	ljmp	$0x08, $1f
+
+	/*
+	 * Load data and stack segments.
+	 */
+1:	movw	$0x10, %dx
+	movw	%dx, %ds
+	movw	%dx, %es
+	movw	%dx, %fs
+	movw	%dx, %gs
+	movw	%dx, %ss
+
+	/*
 	 * Set up the kernel stack.
 	 */
 	movl	$0x200000, %edx
@@ -53,5 +73,65 @@ _start:
 	 */
 1:	hlt
 	jmp	1b
+
+	/*
+	 * The data section.
+	 */
+	.data
+
+	/*
+	 * Descriptor flags, byte 6.
+	 */
+	.equiv	DESC_READABLE,	1 << 1
+	.equiv	DESC_WRITABLE,	1 << 1
+	.equiv	DESC_CODE,	1 << 3
+	.equiv	DESC_USER,	1 << 4
+	.equiv	DESC_PRESENT,	1 << 7
+
+	/*
+	 * Descriptor flags, byte 7.
+	 */
+	.equiv	DESC_32_BIT,	1 << 6
+	.equiv	DESC_4_KB, 	1 << 7
+
+	/*
+	 * The Global Descriptor Table.
+	 */
+	.align	8
+GDT:
+	/*
+	 * The "null" segment -- can't be used, but must exist.
+	 */
+	.zero	8
+
+	/*
+	 * 32-bit flat read-execute user code segment, ring 0.
+	 */
+	.word	0xffff
+	.word	0x0000
+	.byte	0x00
+	.byte	DESC_PRESENT | DESC_USER | DESC_CODE | DESC_READABLE
+	.byte	DESC_4_KB | DESC_32_BIT | 0xf
+	.byte	0x00
+
+	/*
+	 * 32-bit flat read-write user data segment, ring 0.
+	 */
+	.word	0xffff
+	.word	0x0000
+	.byte	0x00
+	.byte	DESC_PRESENT | DESC_USER | DESC_WRITABLE
+	.byte	DESC_4_KB | DESC_32_BIT | 0xf
+	.byte	0x00
+
+GDTR:
+	/*
+	 * The 6-byte structure to load into the GDTR.
+	 *
+	 * The first word is the limit, or size, of the table, in bytes.
+	 * The next dword is the linear (virtual) base address of the table.
+	 */
+	.word	GDTR - GDT
+	.int	GDT
 
 	.end
